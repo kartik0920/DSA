4fd8d67a756f7b8c42cf7f388e5c8dbd4a0f34cc
kartik gile <kartik0920@gmail.com>
2023-10-14 00:12:33 +0530

    lec 35

--- lec35/merge2arr.cpp
+++ lec35/merge2arr.cpp
@@ -1,7 +1,6 @@
-// merging the 2 sorted array
 #include <iostream>
 using namespace std;
-void printArr(int arr[], int size)
+void printArr(int *arr, int size)
 {
     for (int i = 0; i < size; i++)
     {
@@ -9,7 +8,7 @@ void printArr(int arr[], int size)
     }
     cout << endl;
 }
-void merge(int arr1[], int m, int arr2[], int n, int arr3[])
+void merge(int *arr1, int m, int *arr2, int n, int *arr3)
 {
     int i = 0, j = 0, k = 0;
     while (i < m && j < n)
@@ -23,24 +22,24 @@ void merge(int arr1[], int m, int arr2[], int n, int arr3[])
             arr3[k++] = arr2[j++];
         }
     }
-    while (i < n)
+    while (i < m)
     {
         arr3[k++] = arr1[i++];
     }
-    while (j < m)
+
+    while (j < n)
     {
-        arr3[k++] = arr1[j++];
+        arr3[k++] = arr2[j++];
     }
 }
 int main()
 {
-    int arr1[] = {1, 3, 5, 6, 7, 9, 11};
-    int m = sizeof(arr1) / sizeof(arr1[0]);
-    int arr2[] = {2, 2, 4, 6, 8};
-    int n = sizeof(arr2) / sizeof(arr2[0]);
-    int k = m + n;
-    int arr3[k];
+    int m = 5, n = 4;
+    int arr1[m] = {1, 2, 3, 4, 5};
+    int arr2[n] = { 7, 8, 9, 10};
+    int k = m + n, arr3[k];
     merge(arr1, m, arr2, n, arr3);
+    cout << "Array will be:" << endl;
     printArr(arr3, k);
     return 0;
 }
--- lec35/mergesort.cpp
+++ lec35/mergesort.cpp
@@ -1,7 +1,72 @@
 #include <iostream>
 using namespace std;
-int main ()
+void printArr(int *arr, int size)
 {
-    
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+void merge(int *arr, int s, int e)
+{
+    int mid = (s + e) / 2;
+    int l1 = mid - s + 1;
+    int l2 = e - mid;
+    int *first = new int[l1];
+    int *second = new int[l2];
+    int k = s;
+    for (int i = 0; i < l1; i++)
+    {
+        first[i] = arr[k++];
+    }
+    k = mid + 1;
+    for (int i = 0; i < l2; i++)
+    {
+        second[i] = arr[k++];
+    }
+
+    int i = 0, j = 0;
+    k = s;
+    while (i < l1 && j < l2)
+    {
+        if (first[i] < second[j])
+        {
+            arr[k++] = first[i++];
+        }
+        else
+        {
+            arr[k++] = second[j++];
+        }
+    }
+    while (i < l1)
+    {
+        arr[k++] = first[i++];
+    }
+
+    while (j < l2)
+    {
+        arr[k++] = second[j++];
+    }
+}
+void mergesort(int *arr, int s, int e)
+{
+    if (s >= e)
+    {
+        return;
+    }
+    int mid = (s + e) / 2;
+    mergesort(arr, s, mid);
+    mergesort(arr, mid + 1, e);
+
+    merge(arr, s, e);
+}
+int main()
+{
+    int arr[] = {9, 8, 86, 5, 7, 2, 3, 4, 1, 0};
+    int size = 10;
+    mergesort(arr, 0, size - 1);
+    cout << endl;
+    printArr(arr, size);
     return 0;
 }
