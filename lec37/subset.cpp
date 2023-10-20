b895416c1c162a6af553b16deddb0e570abd2ec4
kartik gile <kartik0920@gmail.com>
2023-10-20 12:13:18 +0530

    -

---
+++ lec37/subset.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<vector>
+vector<vector<int>> subset(vector <int> &nums){
+
+}
+using namespace std;
+int main ()
+{
+vector <int> nums={1,2,3};
+subset(nums);
+return 0;
+}
